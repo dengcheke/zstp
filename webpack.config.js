f87aa58decd3064c1bae387f4df4d055950d2df6
dk <dk@abc.com>
2021-12-13 14:49:53 +0800

    change public path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './docs'),
-        publicPath: '/',
+        publicPath: 'https://dengcheke.github.io/zstp/',
     },
     resolve: {
         alias: {
